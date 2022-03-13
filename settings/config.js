6659501f259f540d27317538154af07e2e8dceae
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-03-13 09:45:52 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -7,9 +7,9 @@ global.anticall = false
 global.anti_delete = false
 
 // setting
-global.ownername ="ZeeoneOfc"
-global.ownernumber = "62887435047326"
-global.botname = "Haruka-Bot"
+global.ownername ="Zall"
+global.ownernumber = "6281392128956"
+global.botname = "ROZZ-BOTZ"
 global.thumbnail = fs.readFileSync("./settings/Haruka.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
 global.lolkey = 'e966d3aac613b61dabd35cc6' //register lolhuman.xyz klo mau apikey
